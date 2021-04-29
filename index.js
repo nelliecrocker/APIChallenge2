92ea6963bfa4df8940b1aa76139ea614efff1672
Nellie Crocker <crocker.nellie@gmail.com>
2021-04-29 18:03:05 -0400

    displays image for each letter

--- index.js
+++ index.js
@@ -10,34 +10,37 @@ let baseURL = "http://api.giphy.com/v1/gifs/random?api_key=iPMwiJS956zJ5N4dsf3vQ
 document.getElementById('name-input').addEventListener('keypress', function (event) {
     if (event.key === 'Enter') {
         let name = document.getElementById("name-input").value
-        console.log(name); 
-        //map through the name input and set each letter as a string
+        console.log(name);
+
+        //set each letter as a string
         let nameArray = name.split("")
         console.log(nameArray)
-        event.preventDefault()
-        //conduct search for each index in the array
-        for(letter of nameArray){
+        event.preventDefault() // stops page reload
+
+        //conduct image search for each index in the array
+        for (letter of nameArray) {
             fetch("https://api.giphy.com/v1/gifs/search?api_key=iPMwiJS956zJ5N4dsf3vQwcl2gnNQASC&q=alphabet letter" + `${letter}` + "&limit=25&offset=0&rating=g&lang=en")
-            .then(res => res.json())
-            .then(json => {
-                let giphyImage = json.data.embed_url
-            })
+                .then(res => res.json())
+                .then(json => {
+                    let giphyImage = json.data[0].embed_url
+                    console.log(giphyImage);
+                    // displayGiphy()
+                })
         }
     }
 })
 
-
-
 //assign each index to a card
 
 //function to display each new card
-displayGiphy = (giphy) => {
-    const wrapper = document.getElementById('giphy-letter')
-    let card = document.createElement("div")
-    card.className = "card"
-    let card_giphy = document.createElement("video")
-    card_giphy.className = "card-img-top"
-    card_giphy.src = giphyImage
-    card.appendChild(card_giphy)
-    wrapper.appendChild(card)
-}
+// const displayGiphy = (giphy) => {
+//     const wrapper = document.getElementById('giphy-letter')
+//     let card = document.createElement("div")
+//     card.className = "card"
+//     let card_giphy = document.createElement("video")
+//     card_giphy.className = "card-img-top"
+//     card_giphy.src = URL(giphy.giphyImage) //embed_url    //giphyImage
+//     card.appendChild(card_giphy)
+//     wrapper.appendChild(card)
+// }
+
